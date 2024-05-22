4f0cb2d5e7569f151beae1a43fe8b782ee4bd2c1
aleBeretta <beretta.alessandro06@gmail.com>
2024-05-22 23:37:29 +0200

    faccio cuore incredibile

--- Pagine/PagineWiki/WikiArco.php
+++ Pagine/PagineWiki/WikiArco.php
@@ -4,15 +4,20 @@
 
     $Username=$_SESSION["Username"];
     $NomePag=basename($_SERVER["PHP_SELF"]);
-
-
+    
+    
     $query="SELECT Pagina FROM giocatore 
     JOIN preferiti ON giocatore.Username=preferiti.Username 
     JOIN pagpref ON preferiti.IdPag=pagpref.IdPag
     WHERE giocatore.Username='$Username' AND Pagina='$NomePag'";
 
     $dati = $Connessione -> query($query) or die("ERRORE". $Connessione->error);
-    
+    if ($dati->num_rows == 0){
+        $Pref=false;
+    }
+    else{
+        $Pref=true;
+    }
 ?>
 
 <!DOCTYPE php>
@@ -41,7 +46,17 @@
             ?>
 
             <div class="cuore">
-                <a href=""><img src="../../Immagini/PhpImg/SPCuore.png" alt=""></a>
+                <?php
+                    if($Pref==false) {echo <<<EOD
+                        <a href=""><img src="../../Immagini/PhpImg/SPCuore.png" alt=""></a>
+                        EOD;
+                    }
+                    else{echo <<<EOD
+                        <a href=""><img src="../../Immagini/PhpImg/SPCuoreRosso.png" alt=""></a>
+                        EOD;
+                    }
+                ?>
+                
             </div>
             
             <div class="contenitorePulsanti">
