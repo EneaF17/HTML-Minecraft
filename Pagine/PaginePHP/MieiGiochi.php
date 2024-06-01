f08e7ddfa13a1d759ca9b2dd6331743cd9a653d0
aleBeretta <beretta.alessandro06@gmail.com>
2024-06-01 14:12:53 +0200

    miei giochi

--- Pagine/PaginePHP/MieiGiochi.php
+++ Pagine/PaginePHP/MieiGiochi.php
@@ -5,10 +5,18 @@
 
     $Username = $_SESSION["Username"];
     
-    $Miei = "SELECT Nome,Cognome,Saldo FROM giocatore WHERE Username = '$Username'";
-    $Altri=
+    $QueryNomi= "SELECT Nome, Costo, Deluxe, Versione, Titolo, Descrizione, Foto, Copertina, Elementi FROM giochi WHERE Id_gioco IN (SELECT Id_gioco FROM posseduti WHERE Username = '$Username')";
+    $DatiMiei = $Connessione -> query($QueryNomi) or die("ERRORE QUERY". $Connessione->error);
+    
+
+    $DatiQuery = "SELECT Nome,Cognome,Saldo FROM giocatore WHERE Username = '$Username'";
     $Dati = $Connessione -> query($DatiQuery) or die("ERRORE QUERY". $Connessione->error);
 
+    foreach($Dati as $DatiItem) {
+        $Nome = $DatiItem["Nome"];
+        $Cognome = $DatiItem["Cognome"];
+        $Saldo = $DatiItem["Saldo"];
+        }
 ?>
 
 
@@ -31,49 +39,51 @@
         <div class="BenevenutoTXT">
             <?php echo"<h1>Bentornato $Nome $Cognome</h1> ";?>
         </div>
-            <div class="SPboxSaldo">
-                <h3>IL TUO SALDO É DI : <?php echo "$Saldo" ?> $</h3>
-            </div>
-        <a href="MieiGiochi.php" class="LinkBoxSP">
-            <div class="SPboxLinks">
-                    <img src="../../Immagini/PhpImg/SPChest.png" class="iconaSPHome" alt="">
-                    <div class="SPboxLinksTXT">
-                        <h3>I miei giochi</h3>
-                        <p>Visualizza i tuoi giochi e quelli disponibili nel negozio.</p>
-                    </div>
-            </div>
-        </a>
-        <a href="IlMioAccount.php" class="LinkBoxSP">
-            <div class="SPboxLinks">
-                    <div class="SPboxLinksTXT">
-                        <h3>Il mio account</h3>
-                        <p>Visualizza i tuoi dati per modificarli o aggiornarli</p>
-                    </div>
-                    <img src="../../Immagini/PhpImg/SPAllay.png" class="iconaSPHome" alt="">
-            </div>
-        </a>
-        <a href="Preferiti.php" class="LinkBoxSP">
-            <div class="SPboxLinks">
-                    <img src="../../Immagini/PhpImg/SPCuore.png" class="iconaSPHome" alt="">
-                    <div class="SPboxLinksTXT">
-                        <h3>Preferiti</h3>
-                        <p>Visualizza le pagine salvate nei preferiti</p>
-                    </div>
-            </div>
-        </a>
-        <a href="../../data/LogOut.php" class="LinkBoxSP">
-            <div class="SPboxLinks">
-                    <div class="SPboxLinksTXT">
-                        <h3>LOGOUT</h3>
-                        <p>Esci dal tuo account e torna al menú principale</p>
-                    </div>
-                    <img src="../../Immagini/PhpImg/SPLock.png" class="iconaSPHome" alt="">
-            </div>
-        </a>
+        <div class="SPboxSaldo">
+            <h3>IL TUO SALDO É DI : <?php echo "$Saldo" ?> $</h3>
+        </div>
+        <p>
+            <?php  echo $DatiMiei->num_rows>0?"I tuoi giochi:":"Non hai acquistato nessun gioco" ?>
+        </p>
+        <div class="Box_negozio">
+        <?php 
+            if($DatiMiei->num_rows>0){
+                foreach($DatiMiei as $Gioco)
+                {
+                    $Nomegioco=$Gioco["Nome"];
+                    $Costo=$Gioco["Costo"];
+                    $Deluxe=$Gioco["Deluxe"];
+                    $Versione=$Gioco["Versione"];
+                    $Titolo=$Gioco["Titolo"];
+                    $Foto=$Gioco["Foto"];
+                    $Copertina=$Gioco["Copertina"];
+                    $Elementi=$Gioco["Elementi"];
+                    $Desc=$Gioco["Descrizione"];
+                    if($Nomegioco=="Minecraft") $sfondo="../../Immagini/Negozio/Terra.jpg";
+                    elseif($Nomegioco=="Dungeons") $sfondo="../../Immagini/Negozio/Cobblestone.png";
+                    elseif($Nomegioco=="Legends") $sfondo="../../Immagini/Negozio/Netherrack.png";
+                    echo <<<EOD
+                    <a class="LinkShop" href="Gioco.php?nome_gioco='$Nomegioco'">
+                        <div class="BoxGioco" style="background-image: url($sfondo);">
+                            <div class="ImgSide_Shop">
+                                <img class="fullImg" src="../../Immagini/Negozio/$Foto" alt="">
+                            </div>
+                            <div class="TextSide_Shop">
+                                <h2>$Nomegioco</h2>
+                                <h2 class="Gr">$Costo</h2>
+                            </div>
+                        </div>
+                    </a>
+                    EOD;
+                }
+            }
+        ?>
+        </div>
+        
     </div>
+    </main>
     <footer class="NoMargin">
         <p class="small-text NoMargin">Sito non ufficiale programmato al solo scopo didattico e dimostrativo  da Alessandro Beretta e Enea Fascilla</p>
     </footer>
-    </main>
 </body>
 </php>
